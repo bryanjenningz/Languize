0d53765c75f9485efb8817dee2f448102ea0cfed
Bryan Jennings <bryanjenningz@gmail.com>
2018-01-22 14:53:17 -0800

    move Chat component to a separate page from MessageEdit

---
+++ src/Chat.js
@@ -0,0 +1,165 @@
+import React from "react";
+import { connect } from "react-redux";
+import AppBar from "./AppBar";
+import { changeText, addMessage, startEditingMessage } from "./reducer";
+
+const randomId = () => String(Math.random()).slice(2);
+
+const Chat = ({
+  text,
+  messages,
+  editing,
+  changeText,
+  addMessage,
+  startEditingMessage
+}) => (
+  <div>
+    <AppBar />
+    <div
+      style={{
+        textAlign: "center",
+        maxWidth: 700,
+        margin: "0 auto",
+        height: "100vh",
+        padding: "55px 10px 10px 10px"
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <div style={{ flex: 9, overflow: "auto" }}>
+          {messages.map((m, i) => (
+            <div
+              key={m.id}
+              style={{
+                width: "80%",
+                padding: 30,
+                margin: "20px 0",
+                backgroundColor: "white",
+                fontSize: 20,
+                position: "relative",
+                left: i % 2 === 1 ? "20%" : 0
+              }}
+            >
+              <div
+                style={{
+                  position: "absolute",
+                  top: 10,
+                  left: 15,
+                  cursor: "pointer"
+                }}
+                onClick={e => {
+                  e.stopPropagation();
+                  startEditingMessage(m);
+                }}
+              >
+                ✎
+              </div>
+              {m.audio ? (
+                <div
+                  style={{
+                    position: "absolute",
+                    left: 15,
+                    top: 15,
+                    cursor: "pointer"
+                  }}
+                  onClick={() => {
+                    new Audio(m.audio).play();
+                  }}
+                >
+                  <span role="img" aria-label="play text audio">
+                    🔊
+                  </span>
+                </div>
+              ) : null}
+              {m.translationAudio ? (
+                <div
+                  style={{
+                    position: "absolute",
+                    left: 15,
+                    bottom: 15,
+                    cursor: "pointer"
+                  }}
+                  onClick={() => {
+                    new Audio(m.translationAudio).play();
+                  }}
+                >
+                  <span role="img" aria-label="play translation audio">
+                    🔊
+                  </span>
+                </div>
+              ) : null}
+              {m.text}
+              {m.translation ? (
+                <div>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      borderTop: "1px solid #aaa",
+                      marginTop: 10,
+                      paddingTop: 10
+                    }}
+                  >
+                    <div>{m.translation}</div>
+                  </div>
+                </div>
+              ) : null}
+            </div>
+          ))}
+        </div>
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            height: 50,
+            marginTop: 10
+          }}
+        >
+          <input
+            placeholder="Enter text here"
+            onChange={e => changeText(e.target.value)}
+            value={text}
+            style={{
+              width: "80%",
+              textAlign: "center",
+              fontSize: 20,
+              border: 0
+            }}
+          />
+          <div
+            onClick={() =>
+              addMessage({
+                id: randomId(),
+                text,
+                audio: "",
+                translation: "",
+                translationAudio: ""
+              })
+            }
+            style={{
+              width: "20%",
+              fontSize: 20,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              cursor: "pointer",
+              backgroundColor: "#13c713",
+              color: "white"
+            }}
+          >
+            +
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+const mapState = state => state;
+
+const mapDispatch = {
+  changeText,
+  addMessage,
+  startEditingMessage
+};
+
+export default connect(mapState, mapDispatch)(Chat);
--- src/MessageEdit.js
+++ src/MessageEdit.js
@@ -7,6 +7,37 @@ import {
   cancelEditingMessage
 } from "./reducer";
 
+const recordAudio = () =>
+  new Promise(async resolve => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    const audioChunks = [];
+
+    mediaRecorder.addEventListener("dataavailable", event => {
+      audioChunks.push(event.data);
+    });
+
+    const stop = () =>
+      new Promise(resolve => {
+        mediaRecorder.addEventListener("stop", () => {
+          const audioBlob = new Blob(audioChunks);
+          const audioUrl = URL.createObjectURL(audioBlob);
+          const audio = new Audio(audioUrl);
+          const play = () => audio.play();
+          resolve({ audioBlob, audioUrl, play });
+        });
+
+        mediaRecorder.stop();
+      });
+
+    const start = () => {
+      mediaRecorder.start();
+      return stop;
+    };
+
+    resolve(start);
+  });
+
 const MessageEdit = ({
   editing,
   editText,

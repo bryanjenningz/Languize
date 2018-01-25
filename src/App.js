b6ed2f62f4a78fdba899d66013e9308530501e86
Bryan Jennings <bryanjenningz@gmail.com>
2018-01-25 14:02:31 -0800

    show different routes when route changes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ import Chats from "./Chats";
 import Chat from "./Chat";
 import MessageEdit from "./MessageEdit";
 
-const App = ({ editing, selectedChatID }) => {
-  if (editing) {
-    return <MessageEdit />;
-  } else if (selectedChatID) {
-    return <Chat />;
-  } else {
-    return <Chats />;
+const App = ({ editing, selectedChatID, route }) => {
+  switch (route) {
+    case "Chats":
+      if (editing) {
+        return <MessageEdit />;
+      } else if (selectedChatID) {
+        return <Chat />;
+      } else {
+        return <Chats />;
+      }
+    case "Review":
+      return <div>Review</div>;
+    case "People":
+      return <div>People</div>;
+    case "Profile":
+      return <div>Profile</div>;
+    default:
+      throw new Error(`Invalid route: ${route}`);
   }
 };
 
-export default connect(({ editing, selectedChatID }) => ({
+export default connect(({ editing, selectedChatID, route }) => ({
   editing,
-  selectedChatID
+  selectedChatID,
+  route
 }))(App);
